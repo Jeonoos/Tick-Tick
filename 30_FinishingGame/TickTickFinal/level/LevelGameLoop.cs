b93c077123afda2bd9db76d6b4967d52deed66e6
Jeonoos <j.vreugdenhil2@students.uu.nl>
2017-10-25 16:37:18 +0200

    upload

---
+++ 30_FinishingGame/TickTickFinal/level/LevelGameLoop.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+partial class Level : GameObjectList
+{
+    public override void HandleInput(InputHelper inputHelper)
+    {
+        base.HandleInput(inputHelper);
+        if (quitButton.Pressed)
+        {
+            Reset();
+            GameEnvironment.GameStateManager.SwitchTo("levelMenu");
+        }      
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+
+        TimerGameObject timer = Find("timer") as TimerGameObject;
+
+        Player player = Find("player") as Player;
+        position.X += (-player.Position.X + GameEnvironment.Screen.X / 2- position.X)/GameEnvironment.Screen.X * gameTime.ElapsedGameTime.Milliseconds;
+
+        if (position.X > 0)
+            position.X = 0;
+
+        GameObjectGrid tiles = Find("tiles") as GameObjectGrid;
+        if (position.X < tiles.Objects.GetLength(0) * -72 + GameEnvironment.Screen.X) 
+            position.X = tiles.Objects.GetLength(0) * -72 + GameEnvironment.Screen.X;
+
+        GameObjectList backgrounds = Find("backgrounds") as GameObjectList;
+        backgrounds.Children[0].Position = new Vector2(-position.X, backgrounds.Children[0].Position.Y);
+
+
+        // check if we died
+        if (!player.IsAlive)
+        {
+            timer.Running = false;
+        }
+
+        // check if we ran out of time
+        if (timer.GameOver)
+        {
+            player.Explode();
+        }
+                       
+        // check if we won
+        if (Completed && timer.Running)
+        {
+            player.LevelFinished();
+            timer.Running = false;
+        }
+    }
+
+    public override void Reset()
+    {
+        base.Reset();
+        VisibilityTimer hintTimer = Find("hintTimer") as VisibilityTimer;
+        hintTimer.StartVisible();
+    }
+}
